65367dfbb57e485da9030b2bf70957e3d545490c
unknown <Niswa>
2024-06-12 15:44:16 +0700

    update pertemuan 13

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,29 @@ use App\Http\Controllers\MovieController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/products', [ProductController::class, 'index']);
 
-Route::resource('/movies', MovieController::class);
 
-Route::get('/genres', [GenreController::class, 'index']);
-Route::get('/genres/create', [GenreController::class, 'create']);
-Route::post('/genres', [GenreController::class, 'store']);
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
-Route::get('/genres/{genre}/edit', [GenreController::class, 'edit']);
-Route::put('/genres/{genre}', [GenreController::class, 'update']);
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/reviews', [ReviewController::class, 'index']);
-Route::get('/reviews/create', [ReviewController::class, 'create']);
-Route::post('/reviews', [ReviewController::class, 'store']);
-Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
-Route::get('/reviews/{review}/edit', [ReviewController::class, 'edit']);
-Route::put('/reviews/{review}', [ReviewController::class, 'update']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/movies', MovieController::class);
+    Route::resource('/genres', GenreController::class);
+    Route::resource('/reviews', ReviewController::class);
+});
+
+require __DIR__.'/auth.php';
